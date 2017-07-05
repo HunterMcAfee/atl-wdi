f871f0f6185126af72cce633fe705b6b999219b7
HunterMcAfee <huntmac10@gmail.com>
2017-07-05 10:16:19 -0400

    Added answers

--- morning_exercises/unit_01/javaScript/js-practice/classwork.js
+++ morning_exercises/unit_01/javaScript/js-practice/classwork.js
@@ -10,52 +10,76 @@
 
 // 1. true || false 
 var resOne;
-// Replace This Comment With Your Code
+resOne = true;
 
 // 2. null || ''
 var resTwo;
-// Replace This Comment With Your Code
+resTwo = "";
 
 // 3. 'false' && true
 var resThree;
-// Replace This Comment With Your Code
+resThree = "false";
 
 // 4. !!0
 var resFour;
-// Replace This Comment With Your Code
+resFour = 0;
 
 // 5. !null && !undefined
 var resFive;
-// Replace This Comment With Your Code
+resFive = 10;
 
 /// Flow Control ///
 
 // 6. Write a `while` loop that increases `resSix` by 23 until `resSix` is five
 //    digits long. (Hint: take a look at the .toString() method)
 var resSix;
-// Replace This Comment With Your Code
+resSix = 0;
+var i = 5;
+while (resSix.toString().length <= i) {
+  resSix = resSix + 23;
+}
 
 // 7. Write a `while` loop that increases `resSeven` by 12 when it's strictly
 //    below 95, decreases it by 7 when it's strictly above 105, and stops as
 //    soon as `resSeven` equals or falls between those two values.
 var resSeven;
-// Replace This Comment With Your Code
+resSeven = 0;
+var i = 0;
+While (i < 100) {
+  if (resSeven >= 95 && resSeven <= 105) {
+    break;
+  } else if (resSeven < 95 && resSeven < 105) {
+    resSeven = resSeven + 12;
+  } else if (resSeven > 105) {
+    resSeven = resSeven - 7;
+  }
+}
+
 
 // 8. Write a `for` loop that counts from 0 (inclusive) to 1857 (exclusive) and
 //    calculates the sum of all of those numbers, storing the result in the
 //    variable `resEight`.
 var resEight;
-// Replace This Comment With Your Code
+resEight = 0;
+for (var i = 0; i < 1857; i += 1) {
+  resEight = resEight + i;
+};
 
 // 9. Using a `for` loop, calculate the sum of all multiples of 7 between 0
 //    (inclusive) and 1000 (exclusive); store the result in the variable
 //    `resNine`.
 var resNine;
-// Replace This Comment With Your Code
+resNine = 0;
+for (var i = 0; i < 1000; i +=1) {
+  resNine = resNine + (7*i);
+};
 
 // 10. For numbers 1-10, calculate n^n and then store the sum of all of these values in the variable `resTen`.
 var resTen;
-// Replace This Comment With Your Code
+resTen = 0;
+for (var n = 0; n <= 10; n += 1) {
+  resTen = resTen + Math.pow(n, n);
+}
 
 /// DO NOT EDIT BELOW THIS LINE ///
 module.exports = {
