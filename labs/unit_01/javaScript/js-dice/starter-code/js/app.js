b9858e1548f6ba8e11ec275cffa3c02572be8f52
HunterMcAfee <huntmac10@gmail.com>
2017-07-07 16:46:32 -0400

    Updated with answers from class

--- labs/unit_01/javaScript/js-dice/starter-code/js/app.js
+++ labs/unit_01/javaScript/js-dice/starter-code/js/app.js
@@ -15,3 +15,21 @@ Creating a page where every time the user hits the "Roll Dice" button, the scree
 3) Check to see if the Dice Roll has been hit, if it has run the diceRoll function.
 
 */
+
+document.getElementById("roll-dice").addEventListener("click", diceRoll);
+
+function diceRoll (event) {
+    //Generate two random numbers and assign them to variables, range of numbers should be 1-6
+    var random1 = Math.floor((Math.random() * 6) + 1);
+    var random2 = Math.floor((Math.random() * 6) + 1);
+
+    //Assign string class "dice-" with generated random number to a variable.  Convert random number to string to accomplish this.
+    var firstDie = "dice-" + random1.toString();
+    var secondDie = "dice-" + random2.toString();
+
+    //Update the class of each die by pulling the element from the document using the assigned id
+    document.getElementById("first-die").className = firstDie;
+    document.getElementById("second-die").className = secondDie;
+}
+
+
