8c76b8a96d418eb58a1f0a137c1993b12412d74e
HunterMcAfee <huntmac10@gmail.com>
2017-07-23 02:20:36 -0400

    Added solutions

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -13,11 +13,38 @@ router.get('/new', (req, res) => {
     res.render('new');
 });
 
+router.get('/:id/edit', (req, res) => {
+    const id = req.params.id;
+    const pirateToEdit = pirates[id];
+    res.render('edit', {
+        id: id,
+        data: pirateToEdit
+    });
+});
+
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     res.render('show', {
-        data: pirates[id]
+        data: pirates[id],
+        id: id
     });
+}); 
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    pirates.splice(id, 1);
+    res.redirect('/pirates');
+});
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const pirateToEdit = pirates[id];
+    pirateToEdit.name = req.body.name;
+    pirateToEdit.birthplace = req.body.birthplace;
+    pirateToEdit.death_year = req.body.death_year;
+    pirateToEdit.base = req.body.base;
+    pirateToEdit.nickname = req.body.nickname;
+    res.redirect(`/pirates/${id}`);
 });
 
 router.post('/', (req, res) => {
