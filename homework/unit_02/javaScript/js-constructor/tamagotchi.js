dcbd85329aa58704e89f32b8322315375ab606d0
HunterMcAfee <huntmac10@gmail.com>
2017-07-24 17:54:26 -0400

    part 3: added new methods

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -13,12 +13,24 @@ class Tamagotchi {
         this.foodInTummy--;
         console.log(`${this.propertyName} is crying. WAHH!!!`);
     }
+    puke () {
+        this.foodInTummy--;
+        console.log(`${this.foodInTummy} WAHH!!!`)
+    }
+    yawn () {
+        this.restedness--;
+        console.log(`${this.propertyName} has current restedness of: ${this.restedness}`);
+    }
 }
 
 //create new Tamagotchis
 let david = new Tamagotchi("David", "Snail");
 let richard = new Tamagotchi("Richard", "Gas Monkey");
-david.cry();
-richard.cry();
+david.cry()
+david.puke()
+david.yawn();
+richard.cry()
+richard.puke()
+richard.yawn();
 
 //test out your Tamagotchies below via console.logs
