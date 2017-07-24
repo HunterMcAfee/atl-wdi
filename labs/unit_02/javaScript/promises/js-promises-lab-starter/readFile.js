6a7d6a9ecd955cfd020a9852f7619857492bbecb
HunterMcAfee <huntmac10@gmail.com>
2017-07-24 17:18:49 -0400

    Added answers. Incomplete

--- labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
+++ labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
@@ -4,19 +4,37 @@ const fs = require('fs');
 
 let inFile = process.argv[2];
 
-fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
-  if (error) {
-    console.error(error);
-  }
+// fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+//   if (error) {
+//     console.error(error);
+//   }
 
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
+//   // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
 
-  let lines = content.split('\n');
+//   let lines = content.split('\n');
 
-  // clean up the array by removing the empty line
-  lines.pop();
+//   // clean up the array by removing the empty line
+//   lines.pop();
 
-  lines.forEach(function(line) {
-    console.log('Hello, ' + line + '!');
+//   lines.forEach(function(line) {
+//     console.log('Hello, ' + line + '!');
+//   });
+// });
+
+var readFilePromise = new Promise((resolve, reject) => {
+    fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+      if(err) {
+        reject(err);
+      }
+      resolve(content);
+    })
+});
+
+readFilePromise
+  .then(data => data.split('\n'));
+  .then(lines => {
+    lines.pop();
+    lines.forEach(function(line) {
+    console.log('Hello, ' + line + '!'); 
   });
 });
