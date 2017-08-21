5ee73e69d48a854877303a21a18d58a8e1c68682
HunterMcAfee <huntmac10@gmail.com>
2017-08-21 17:56:53 -0400

    Arrays skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,42 +1,51 @@
 
-# CHALLENGE ONE
-a = true
-b = false
-puts a.equal?(b)
-puts b.equal?(!a)
+# # CHALLENGE ONE
+# a = true
+# b = false
+# puts a.equal?(b)
+# puts b.equal?(!a)
 
-# CHALLENGE TWO
-example = nil
-p example
-puts "#{example} nil was here"
-print example
-puts example.class
+# # CHALLENGE TWO
+# example = nil
+# p example
+# puts "#{example} nil was here"
+# print example
+# puts example.class
 
-# CHALLENGE THREE
-d = 10
-puts d
-e = 25.0
-puts e
-d = e
-puts d
+# # CHALLENGE THREE
+# d = 10
+# puts d
+# e = 25.0
+# puts e
+# d = e
+# puts d
 
-# CHALLENGE FOUR
-christine = "Hi, I'm Christine"
-likesTo = "long walks on the beach"
-tims = christine
-puts "#{tims} and I like #{likesTo}"
-g = "2"
-gToNumber = 2
-puts g.to_i + gToNumber
+# # CHALLENGE FOUR
+# christine = "Hi, I'm Christine"
+# likesTo = "long walks on the beach"
+# tims = christine
+# puts "#{tims} and I like #{likesTo}"
+# g = "2"
+# gToNumber = 2
+# puts g.to_i + gToNumber
 
-# CHALLENGE FIVE
-puts "Put in an integer"
-    number = gets.chomp.to_i
-    puts number * 2
+# # CHALLENGE FIVE
+# puts "Put in an integer"
+#     number = gets.chomp.to_i
+#     puts number * 2
 
-puts "What is your name?"
-    name = gets.chomp
-puts "What do you like to do?"
-    likes_to_do = gets.chomp
-print "#{name} likes to #{likes_to_do}."
+# puts "What is your name?"
+#     name = gets.chomp
+# puts "What do you like to do?"
+#     likes_to_do = gets.chomp
+# print "#{name} likes to #{likes_to_do}."
 
+# CHALLENGE SIX
+myFirstArray = []
+myFirstArray = ["this", "is", "an", "array", "son", 1, 2, 3, 4]
+puts myFirstArray.last
+puts myFirstArray.first
+myFirstArray[1] = "Joe"
+myFirstArray[2] = 3
+myFirstArray.push(true)
+puts myFirstArray[3].class
