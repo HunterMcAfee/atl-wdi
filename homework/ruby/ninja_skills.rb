96d086a33ff2537f80595c8e2904b53b29b36591
HunterMcAfee <huntmac10@gmail.com>
2017-08-21 17:30:28 -0400

    2. Nil skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,4 +1,13 @@
+
+# CHALLENGE ONE
 a = true
 b = false
 puts a.equal?(b)
 puts b.equal?(!a)
+
+# CHALLENGE TWO
+example = nil
+p example
+puts "#{example} nil was here"
+print example
+puts example.class
