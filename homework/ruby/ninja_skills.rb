327ec283c2e6fe083f3a48f5017362790e60bb6c
HunterMcAfee <huntmac10@gmail.com>
2017-08-21 17:37:22 -0400

    4. String skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -12,10 +12,19 @@ puts "#{example} nil was here"
 print example
 puts example.class
 
-# CHALLEGE THREE
+# CHALLENGE THREE
 d = 10
 puts d
 e = 25.0
 puts e
 d = e
 puts d
+
+# CHALLENGE FOUR
+christine = "Hi, I'm Christine"
+likesTo = "long walks on the beach"
+tims = christine
+puts "#{tims} and I like #{likesTo}"
+g = "2"
+gToNumber = 2
+puts g.to_i + gToNumber
